8eb20666ac3eff1c7519c9d9a6e158581b8152ba
Myrta R <80482586+Myrtaru@users.noreply.github.com>
2021-05-22 09:42:25 +0200

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,16 +6,15 @@ return (
     <h3>{props.meaning.partOfSpeech}</h3>
     {props.meaning.definitions.map(function (definition, index){
       return (
-      <div key={index}>
-        <p>
-          {definition.definition}
-          <br/>
-          <span>
-          {definition.example}
-          </span>
-        </p>
-      </div>
-    ); 
+        <div key={index}>
+          <p>
+            <strong>Definition</strong> {definition.definition}
+            <br />
+            <strong>Example </strong>
+            <em>{definition.example}</em>
+          </p>
+        </div>
+      ); 
     })}
   </div>
 );
